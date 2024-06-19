c27ee7c0e31ad5d342ce8df6cfc9b1fc77927116
unknown <bgsdewoo20@gmail.com>
2024-06-19 21:47:35 +0700

    JSX,belajar mereturn yg ada didalam variabel

--- react/src/components/Article.jsx
+++ react/src/components/Article.jsx
@@ -1,5 +1,27 @@
 function Article() {
-    return <div>Ini adalah component pertama</div>
+    const nama = 'Bagus'
+    const titles = ['HTML','JS','CSS']
+    // titles.map(item => item)
+    return (
+        <>
+        <div>{nama}</div>
+        {/* <div>
+            {titles.map(title => <div>{title}</div>)}  */}
+            {/* kalo me returnnya satu elemen menggunakan ini */}
+        {/* </div> */}
+        
+        <div>{titles.map(title => {
+            return (
+                <>
+                <div>{title}</div>
+                <div>{title}</div>
+                {/* jika ingin mereturn dua elemen gunakanlah cara ini! */}
+                </>
+            )
+        })}</div>
+        </>
+    )
+    // return <div>{nama}</div> //ini digunakakn ketika kita ingin meretrun satu elemen saja
 }
 
 export default Article
