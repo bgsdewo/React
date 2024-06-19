86b124a3138684ce8fe0e507a56af297d456436c
unknown <bgsdewoo20@gmail.com>
2024-06-19 22:17:33 +0700

    Props pada Child Component, serta membuat component menjadi reusable dengan props

--- react/src/App.jsx
+++ react/src/App.jsx
@@ -3,7 +3,11 @@ import Article from './components/Article'
 function App() {
   return (
       <div className="App">
-        <Article />
+        <Article name="Bagus" titles={["HTML","React JS","CSS"]}/>
+        <br />
+        <Article name="Lala" titles={["Node JS","Vue JS","CSS"]}/>
+
+        {/* <Article name="Bagus" /> */}
       </div>
   )
 }
--- react/src/components/Article.jsx
+++ react/src/components/Article.jsx
@@ -1,26 +1,15 @@
-function Article() {
-    const nama = 'Bagus'
-    const titles = ['HTML','JS','CSS']
-    // titles.map(item => item)
+function Article(props) {
+
     return (
         <>
-        <div>{nama}</div>
-        {/* <div>
-            {titles.map(title => <div>{title}</div>)}  */}
-            {/* kalo me returnnya satu elemen menggunakan ini */}
-        {/* </div> */}
-        
-        <div>{titles.map(title => {
-            return (
-                <>
-                <div>{title}</div>
-                <div>{title}</div>
-                {/* jika ingin mereturn dua elemen gunakanlah cara ini! */}
-                </>
-            )
-        })}</div>
+            <div>{props.name}</div>
+            <div>
+                {props.titles.map(title => {
+                    return <div>{title}</div>;
+                })}
+            </div>
         </>
-    )
+    );
     // return <div>{nama}</div> //ini digunakakn ketika kita ingin meretrun satu elemen saja
 }
 
