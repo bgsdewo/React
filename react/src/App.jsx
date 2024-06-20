c7ebd62f2cc477d1f846e6a3e2cab8bee6225c9a
unknown <bgsdewoo20@gmail.com>
2024-06-20 19:50:33 +0700

    Conditional Rendering 2

--- react/src/App.jsx
+++ react/src/App.jsx
@@ -1,10 +1,8 @@
 import './App.css'
 import Home from './pages'
 function App() {
-  const value = true
   return (
     <div className="App">
-        {value ? 'benar' :'salah'}
         <Home />
       </div>
   )
--- react/src/components/Article.jsx
+++ react/src/components/Article.jsx
@@ -1,10 +1,20 @@
+//rendering dlaam component
+const ArticleStatus = ({isNew}) => {
+    return isNew && <span>Baru</span>
+}
 
-
+//cara baru utk merendering di luar
+const NewArticle = () => {
+    return <span>-- Baru</span>
+}
 function Article(props) {
     return (
         <>
             <h3>{props.title}</h3>
-            <small>Date: {props.date}, tags: {props.tags && props.tags.length > 0 ? props.tags.join(", ") : "No tags"}</small>
+            <small>Date: {props.date}, tags: {props.tags && props.tags.length > 0 ? props.tags.join(", ") : "No tags"}
+                <ArticleStatus isNew={props.isNew}/>
+                {props.isNew && <NewArticle />}
+            </small>
         </>
     );
 }
