94ab1a7728d88eeaea2e75f8665d3d2a26a3019e
unknown <bgsdewoo20@gmail.com>
2024-06-23 10:42:07 +0700

    Dynamic Parameter Route dan Menampilkan Artikel

--- react/src/router/index.jsx
+++ react/src/router/index.jsx
@@ -2,6 +2,7 @@ import {createBrowserRouter} from "react-router-dom"
 import RootLayout from "../Layouts/RootLayout"
 import Home from "../pages/Index"
 import Blog from "../pages/blogs"
+import Post from "../pages/blogs/_id"
 import About from "../pages/About"
 
 
@@ -17,6 +18,10 @@ export const router = createBrowserRouter([
             {
                 path: "/blog",
                 element: <Blog />,
+            },
+            {
+                path: "/blog/:id",
+                element: <Post />,
             },
             {
                 path: "/about",
